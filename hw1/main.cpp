57059cfd0aecf87ec800a89f032ac7d0635c10b1
bjorn <lxxorz@gmail.com>
2023-11-12 20:33:55 +0800

    feat: add rotation matrix

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -19,14 +19,28 @@ Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
     return view;
 }
 
+float to_radian(float degree)
+{
+    return degree * MY_PI / 180.0f;
+}
+
 Eigen::Matrix4f get_model_matrix(float rotation_angle)
 {
+
     Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
 
     // TODO: Implement this function
     // Create the model matrix for rotating the triangle around the Z axis.
     // Then return it.
+    /**
+     * https://excalidraw.com/#json=rHuSwx7q2NQGeUN0bQ4zN,FHz70z-axN1yAuIBi6AFUA
+    */
+    model << cos(to_radian(rotation_angle)), -sin(to_radian(rotation_angle)), 0, 0,
+        sin(to_radian(rotation_angle)), cos(to_radian(rotation_angle)), 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1;
 
+    std::cout << model << std::endl;
     return model;
 }
 
@@ -44,16 +58,18 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
     return projection;
 }
 
-int main(int argc, const char** argv)
+int main(int argc, const char **argv)
 {
     float angle = 0;
     bool command_line = false;
     std::string filename = "output.png";
 
-    if (argc >= 3) {
+    if (argc >= 3)
+    {
         command_line = true;
         angle = std::stof(argv[2]); // -r by default
-        if (argc == 4) {
+        if (argc == 4)
+        {
             filename = std::string(argv[3]);
         }
     }
@@ -72,7 +88,8 @@ int main(int argc, const char** argv)
     int key = 0;
     int frame_count = 0;
 
-    if (command_line) {
+    if (command_line)
+    {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
         r.set_model(get_model_matrix(angle));
@@ -88,7 +105,8 @@ int main(int argc, const char** argv)
         return 0;
     }
 
-    while (key != 27) {
+    while (key != 27)
+    {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
         r.set_model(get_model_matrix(angle));
@@ -102,13 +120,15 @@ int main(int argc, const char** argv)
         cv::imshow("image", image);
         key = cv::waitKey(10);
 
-        std::cout << "frame count: " << frame_count++ << '\n';
+        // std::cout << "frame count: " << frame_count++ << '\n';
 
-        if (key == 'a') {
-            angle += 10;
+        if (key == 'a')
+        {
+            angle += 45;
         }
-        else if (key == 'd') {
-            angle -= 10;
+        else if (key == 'd')
+        {
+            angle -= 45;
         }
     }
 
