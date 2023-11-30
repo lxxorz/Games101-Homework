a6a7df76708dabeeb453d2cfe50c23dad2df2df2
bjorn <lxxorz@gmail.com>
2023-11-30 20:08:40 +0800

    fix: add todo

--- hw2/rasterizer.cpp
+++ hw2/rasterizer.cpp
@@ -136,8 +136,12 @@ void rst::rasterizer::rasterize_triangle(const Triangle& t) {
                 float w_reciprocal = 1.0/(alpha / v[0].w() + beta / v[1].w() + gamma / v[2].w());
                 float z_interpolated = alpha * v[0].z() / v[0].w() + beta * v[1].z() / v[1].w() + gamma * v[2].z() / v[2].w();
                 z_interpolated *= w_reciprocal;
-                // TODO : set the current pixel (use the set_pixel function) to the color of the triangle (use getColor function) if it should be painted.
-                set_pixel(Vector3f(x, y, z_interpolated), t.getColor());
+                // FIXME 修复z_interpolated值错误
+                if(z_interpolated < depth_buf[get_index(x, y)]) {
+                    depth_buf[get_index(x, y)] = z_interpolated;
+                    // TODO : set the current pixel (use the set_pixel function) to the color of the triangle (use getColor function) if it should be painted.
+                    set_pixel(Vector3f(x, y, z_interpolated), t.getColor());
+                }
             }
         }
     }
